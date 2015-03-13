bfc2ce391cf6ae9e49a963facb7bf1d87abec9ed
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-03-13 19:19:13 +0100

    Refactor names of tests

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,42 +7,42 @@ import org.junit.Test;
 public class BinarySearchTest {
 
 		@Test
-		public void searchElementIsInSeq() {
+		public void searchElement_IsInSeq() {
 			int[] seq = {1};
 			SearchResult result = BinarySearch.search(1, seq);		
 			assertTrue(result.isFound());
 		}
 		
 		@Test
-		public void searchElementIsntInSeq() {
+		public void searchElement_IsntInSeq() {
 			int[] seq = {1};
 			SearchResult result = BinarySearch.search(5, seq);		
 			assertFalse(result.isFound());
 		}
 		
 		@Test
-		public void search_manyElement_firstInSeq() {
+		public void searchManyElements_FirstInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(2, seq);		
 			assertTrue(result.isFound());			
 		}
 		
 		@Test
-		public void search_manyElement_lastInSeq() {
+		public void searchManyElements_LastInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(5, seq);		
 			assertTrue(result.isFound());			
 		}
 	
 		@Test
-		public void search_manyElement_middleInSeq() {
+		public void searchManyElements_MiddleInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(3, seq);		
 			assertTrue(result.isFound());			
 		}
 		
 		@Test
-		public void binarySearch_search_manyElement_notInSeq() {
+		public void searchManyElement_nNotInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(4, seq);		
 			assertFalse(result.isFound());			
