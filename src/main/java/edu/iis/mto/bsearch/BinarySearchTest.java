a0d49728ab9067b54b19be64cea2da7988209a7c
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-03-19 19:15:30 +0100

    Added another tests + refactor

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,45 +7,66 @@ import org.junit.Test;
 public class BinarySearchTest {
 
 		@Test
-		public void searchElement_IsInSeq() {
+		public void searchElementInSeq_IsInSeq() {
 			int[] seq = {1};
 			SearchResult result = BinarySearch.search(1, seq);		
 			assertThat(true, is(result.isFound()));
 		}
 		
 		@Test
-		public void searchElement_IsntInSeq() {
+		public void searchElementInSeq_IsntInSeq() {
 			int[] seq = {1};
 			SearchResult result = BinarySearch.search(5, seq);		
 			assertThat(false, is(result.isFound()));
 		}
 		
 		@Test
-		public void searchManyElements_FirstInSeq() {
+		public void searchManyElementsInSeq_FirstInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(2, seq);		
 			assertThat(true, is(result.isFound()));		
 		}
 		
 		@Test
-		public void searchManyElements_LastInSeq() {
+		public void searchManyElementsInSeq_LastInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(5, seq);		
 			assertThat(true, is(result.isFound()));		
 		}
 	
 		@Test
-		public void searchManyElements_MiddleInSeq() {
+		public void searchManyElementsInSeq_MiddleInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(3, seq);		
 			assertThat(true, is(result.isFound()));			
 		}
 		
 		@Test
-		public void searchManyElement_nNotInSeq() {
+		public void searchManyElementInSeq_nNotInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(4, seq);		
 			assertThat(false, is(result.isFound()));			
+		}
+		
+//		@Test(expected = IllegalArgumentException.class)
+//		public void searchNoElemntsInSeq_shouldThrowExeption() {
+//			int[] seq = {};
+//			SearchResult result = BinarySearch.search(4, seq);		
+//			assertThat(false, is(result.isFound()));			
+//		}
+		
+		@Test
+		public void searchManyElementInSeqTheSame_shouldThrowExeption() {
+			int[] seq = {2,2,2};
+			SearchResult result = BinarySearch.search(2, seq);		
+			assertThat(false, is(result.isFound()));			
+		}
+		
+		@Test
+		public void searchManyElementInSeqWithMinusElemnts_shouldFoundInSeq() {
+			int[] seq = {-2,-1,2};
+			SearchResult result = BinarySearch.search(-2, seq);		
+			assertThat(true, is(result.isFound()));			
 		}
 
 }
