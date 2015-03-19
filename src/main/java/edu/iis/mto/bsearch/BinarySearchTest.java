1840d2bdc5e57057ba2695ad8c481d22c99665bf
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-03-19 18:56:46 +0100

    Refactorized BinarySearchTest (used assertThat in test)

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,7 +1,7 @@
 package edu.iis.mto.bsearch;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.core.Is.is;
 import org.junit.Test;
 
 public class BinarySearchTest {
@@ -10,42 +10,42 @@ public class BinarySearchTest {
 		public void searchElement_IsInSeq() {
 			int[] seq = {1};
 			SearchResult result = BinarySearch.search(1, seq);		
-			assertTrue(result.isFound());
+			assertThat(true, is(result.isFound()));
 		}
 		
 		@Test
 		public void searchElement_IsntInSeq() {
 			int[] seq = {1};
 			SearchResult result = BinarySearch.search(5, seq);		
-			assertFalse(result.isFound());
+			assertThat(false, is(result.isFound()));
 		}
 		
 		@Test
 		public void searchManyElements_FirstInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(2, seq);		
-			assertTrue(result.isFound());			
+			assertThat(true, is(result.isFound()));		
 		}
 		
 		@Test
 		public void searchManyElements_LastInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(5, seq);		
-			assertTrue(result.isFound());			
+			assertThat(true, is(result.isFound()));		
 		}
 	
 		@Test
 		public void searchManyElements_MiddleInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(3, seq);		
-			assertTrue(result.isFound());			
+			assertThat(true, is(result.isFound()));			
 		}
 		
 		@Test
 		public void searchManyElement_nNotInSeq() {
 			int[] seq = {2,3,5};
 			SearchResult result = BinarySearch.search(4, seq);		
-			assertFalse(result.isFound());			
+			assertThat(false, is(result.isFound()));			
 		}
 
 }
